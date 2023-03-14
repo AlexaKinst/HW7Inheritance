d2125afe2e4d354b3f607a34aca7357985adf42b
alexa <alexandra.pilina97@gmail.com>
2023-03-14 02:07:19 +0200

    First variant

---
+++ src/Cat.java
@@ -0,0 +1,28 @@
+public class Cat extends Animal {
+
+    static int cats = 0;
+
+    public Cat(String name) {
+        super(200, 0);
+        this.name = name;
+        cats++;
+    }
+
+    public static int getCats() {
+        return cats;
+    }
+
+    public void run(int distance) {
+        if (distance <= 200) {
+            System.out.println(this.name + " пробіг " + distance + " метрів.");
+        } else {
+            System.out.println(this.name + " заморився і ліг відпочити.");
+        }
+    }
+
+    @Override
+    public void swim(int distance) {
+        System.out.println(this.name + " не вміє плавати");
+    }
+}
+
---
+++ src/Dog.java
@@ -0,0 +1,31 @@
+public class Dog extends Animal {
+
+    static int dogs = 0;
+
+    public Dog(String name) {
+        super(500, 10);
+        this.name = name;
+        dogs++;
+    }
+
+    public static int getDogs() {
+        return dogs;
+    }
+
+    public void run(int distance) {
+        if (distance <= 500) {
+            System.out.println(this.name + " пробігла " + distance + " метрів.");
+        } else {
+            System.out.println(this.name + " не може витримати таку нагрузку, тому вирішила піти поїсти.");
+        }
+    }
+
+    @Override
+    public void swim(int distance) {
+        if (distance <= 10) {
+            System.out.println(this.name + " проплила " + distance + " метрів.");
+        } else {
+            System.out.println("Не так вже й " + this.name + " любить плавати.");
+        }
+    }
+}
